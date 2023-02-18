a8020df3c98600e603b221dba328c741a14b0c39
longshiine <dkel03@korea.ac.kr>
2023-02-18 17:21:20 +0900

    feat: loading css
    Signed-off-by: longshiine <dkel03@korea.ac.kr>

--- src/components/CareerInput.tsx
+++ src/components/CareerInput.tsx
@@ -146,7 +146,7 @@ export default function CareerInput() {
               <div role="status" className="mt-10 flex justify-center">
                 <svg
                   aria-hidden="true"
-                  className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-sky-400"
+                  className="w-8 h-8 mr-2 text-gray animate-spin dark:text-gray fill-seer"
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
